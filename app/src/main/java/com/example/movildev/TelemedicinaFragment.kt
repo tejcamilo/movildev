2cab90957e66d0b7db13de2eca7ae772dc371363
tejcamilo <ctejada@twilio.com>
2025-04-21 20:51:57 -0500

    Implementar navegación a cita

--- app/src/main/java/com/example/movildev/TelemedicinaFragment.kt
+++ app/src/main/java/com/example/movildev/TelemedicinaFragment.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class TelemedicinaFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_telemedicina, container, false)
+        val view = inflater.inflate(R.layout.fragment_telemedicina, container, false)
+        val startButton = view.findViewById<Button>(R.id.start)
+
+        startButton.setOnClickListener {
+           view.findNavController().navigate(R.id.action_telemedicinaFragment_to_citaFragment)
+        }
+        return view
     }
 }
