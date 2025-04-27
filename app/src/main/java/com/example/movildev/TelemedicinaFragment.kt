71993e018cd080f837a6977f25745e96a32f73c8
tejcamilo <ctejada@twilio.com>
2025-04-26 20:08:29 -0500

    Agregar Toast a citas inactivas con onClickListener

--- app/src/main/java/com/example/movildev/TelemedicinaFragment.kt
+++ app/src/main/java/com/example/movildev/TelemedicinaFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 
@@ -17,6 +19,11 @@ class TelemedicinaFragment : Fragment() {
         startButton.setOnClickListener {
            view.findNavController()
                .navigate(R.id.action_telemedicinaFragment_to_salaFragment)
+        }
+        val frame1 = view.findViewById<LinearLayout>(R.id.frame1)
+        frame1.setOnClickListener {
+            val text = "El acceso a la cita estará disponible 5 minutos antes de la hora programada"
+            Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
         }
         return view
     }
